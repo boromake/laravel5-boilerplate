02525c3744391d534abd11f32f6a8507bba20650
kevin kawaguchi <kevinkawaguchi@gmail.com>
2016-10-05 15:52:46 -0700

    Add custom Blade directive '@icon'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,28 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		//
+		/*
+		|--------------------------------------------------------------------------
+		| Custom Blade Directives
+		|--------------------------------------------------------------------------
+		*/
+
+		/**
+		 * Create a new @icon Blade directive
+		 *
+		 * Tip: Whatever you pass for $icon_name is put inside the 'class' attribute, so you can
+		 * pass css classes as well.
+		 *
+		 * @uses @icon(trash-o fa-lg) No quotes needed to denote string
+		 */
+		\Blade::directive('icon', function($icon_name)
+		{
+			// need to strip paranthesis because of weird behavior
+			// https://github.com/laravel/framework/issues/7349
+			$icon_name = trim($icon_name, '()');
+
+			return "<?php echo \"<i class=\\\"fa fa-$icon_name\\\" aria-hidden=\\\"true\\\"></i>\" ?>";
+		});
 	}
 
 	/**
