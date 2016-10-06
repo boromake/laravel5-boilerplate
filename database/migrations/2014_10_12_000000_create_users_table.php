6b058309b85064372c2fe4dff1ceb35d89c757d3
kevin kawaguchi <kevinkawaguchi@gmail.com>
2016-10-06 12:38:53 -0700

    Add user migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,30 +6,64 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		/********************************************
+		 * USERS
+		 ********************************************/
+		Schema::connection('mysql_admin')->create('users', function (Blueprint $table) {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('users');
-    }
+			$table->increments('id');
+			$table->uuid('uuid')->unique();
+			$table->string('email')->unique();
+			$table->string('display_name')->nullable();
+			$table->string('password');
+			$table->tinyInteger('account_type')->default(1);
+
+			$table->rememberToken();
+			$table->timestamps();
+
+			$table->softDeletes();
+		});
+
+
+		/********************************************
+		 * USER_LOGIN_HISTORY
+		 ********************************************/
+		Schema::connection('mysql_admin')->create('user_login_history', function (Blueprint $table)
+		{
+			$table->increments('id');
+			$table->uuid('user_id');
+
+			$table->string('ip')->nullable();
+			$table->boolean('used_login_cookie')->nullable();
+			$table->integer('minutes_since_last_login')->nullable();
+			$table->string('server_ip')->nullable();
+			$table->string('server_name')->nullable();
+
+			$table->timestamps();
+
+			$table->index('created_at');
+			$table->foreign('user_id')
+				->references('uuid')->on('users')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::connection('mysql_admin')->dropIfExists('user_login_history');
+		Schema::connection('mysql_admin')->dropIfExists('users');
+	}
 }
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -6,27 +6,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePasswordResetsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at')->nullable();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		/********************************************
+		 * USER_PASSWORD_RESETS
+		 * This is defined by Laravel
+		 ********************************************/
+		Schema::connection('mysql_admin')->create('user_password_resets', function (Blueprint $table) {
+			$table->string('email')->index();
+			$table->string('token')->index();
+			$table->timestamp('created_at')->nullable();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('password_resets');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::connection('mysql_admin')->dropIfExists('user_password_resets');
+	}
 }
